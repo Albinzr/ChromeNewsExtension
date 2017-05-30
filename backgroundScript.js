5d7a8d5b2bb541f08533052829fcc838c42bfc03
albin.git <albin.git@gmail.com>
2017-05-30 11:58:25 +0530

    search text caped

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,12 +1,12 @@
 chrome.contextMenus.create({
-    title: "search with Quintype",
-    contexts: ["selection"],
-    onclick: search
+	title: "Search with Quintype",
+	contexts: ["selection"],
+	onclick: search
 });
 
 function search(data) {
-    var selectedText = data.selectionText
-    chrome.tabs.create({
-        url: "https://thequint.com/search?q=" + selectedText
-    })
+	var selectedText = data.selectionText;
+	chrome.tabs.create({
+		url: "https://thequint.com/search?q=" + selectedText
+	});
 }
